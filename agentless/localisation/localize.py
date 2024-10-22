148891bf4d48f65ea9d0580e404ffb51852a1d2a
Masmoudi Aziz <azizmasmoudi019@gmail.com>
2024-10-22 13:09:48 +0100

    fix: logging

--- agentless/localisation/localize.py
+++ agentless/localisation/localize.py
@@ -22,7 +22,7 @@ FILES_TO_USE = [
 ]
 
 # Set up logging
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 def filter_files(structure: dict, files: list):
